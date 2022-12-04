e333537e98ff297c3b3b52a5ca6fabc960a6f969
Natalie Adams <natalie@allostech.com>
2022-12-04 14:51:25 -0600

    missing std::

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -720,7 +720,7 @@ pub fn set_share_rdp(enable: bool) {
 pub fn get_active_username() -> String {
     let name = crate::username();
     let envName = std::env::var("RUSTDESK_USERNAME");
-    if envName != "" {
+    if envName.is_empty() {
         name = envName;
     }
     if name != "SYSTEM" {
